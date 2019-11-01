86355a619611c27ac5c4b5adbaf1eb4cf0b01141
Jerry Sievert <code@legitimatesounding.com>
2019-10-31 17:40:01 -0700

    force save on reset

--- src/controller/Sequencer.cpp
+++ src/controller/Sequencer.cpp
@@ -64,6 +64,7 @@ SequencerModule::SequencerModule() {
 
 void SequencerModule::doReset() {
   currentCount = -1;
+  savePattern(currentPlay);
   setPlay(programs[0]);
   currentGate = -1;
   currentPosition = 0;
